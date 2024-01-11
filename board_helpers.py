a33da04e649082455229fd0c7dd34669aa73af44
wtoth <wtoth@umich.edu>
2024-01-11 14:32:42 -0500

    Created initial board construction template

---
+++ board_helpers.py
@@ -0,0 +1,7 @@
+
+# this file is for board construction helpers
+# One of the thing I would like to do is to automate the construction of the opponents board creation 
+
+def computer_board_construction():
+    pass
+
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from board import Board
+import board_helpers
 
 def main():
     #board construction
+    player_board = Board()
+    pieces = []
 
+    #place pieces loop
+    for piece in pieces:
+        placement_result = False
+        while not placement_result:
+            placement_result = player_board.add_ship()
+
+    computer_board = board_helpers.computer_automated_board_construction()
+    
     #game loop
 
-    
+
     pass
 
 
