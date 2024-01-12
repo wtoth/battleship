ac95971ea653c3f8b6bbc1af51f16a6e20ae4a59
wtoth <wtoth@umich.edu>
2024-01-11 20:18:45 -0500

    Computer board construction

--- board_helpers.py
+++ board_helpers.py
@@ -1,7 +1,27 @@
+from board import Board
+from random import randrange, randint
 
 # this file is for board construction helpers
 # One of the thing I would like to do is to automate the construction of the opponents board creation 
 
 def computer_board_construction():
-    pass
+    board = Board()
 
+    pieces = [5, 4, 3, 3, 2]
+
+    #place pieces loop
+    for piece in pieces:
+        placement_result = False
+        while not placement_result:
+            row = randrange(10)
+            col = randrange(10)
+            orientation = randint(0,1)
+            if orientation == 1:
+                orientation = "v"
+            else:
+                orientation = "h"
+            placement_result = board.add_ship([row, col], piece, orientation) 
+    return board
+
+test_board = computer_board_construction()
+test_board.display_board()
